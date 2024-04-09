f522468fe4376273769f151f7e3a227b45f197c0
Paul Straus <paul.straus@gmail.com>
2024-04-09 10:48:55 -0500

    saving progress

--- test/TestParticle.cpp
+++ test/TestParticle.cpp
@@ -1,12 +1,12 @@
-#include <Particle.cuh>
+//#include <Particle.cuh>
 #include <googletest/include/gtest/gtest.h>
 
-TEST(TestParticle, turn_on)
-{
-  Particle<3> p;
-  Eigen::Matrix<float, 3, 1> state;
-  state << 1, 2, 3;
-  // p.setState(state);
-  // Eigen::Matrix<float, 3, 1> state2 = p.getState();
-  // ASSERT_EQ(state, state2);
-}
+// TEST(TestParticle, turn_on)
+//{
+//   Particle<3> p;
+//   Eigen::Matrix<float, 3, 1> state;
+//   state << 1, 2, 3;
+//  p.setState(state);
+//  Eigen::Matrix<float, 3, 1> state2 = p.getState();
+//  ASSERT_EQ(state, state2);
+//}
